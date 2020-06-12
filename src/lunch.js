627ba4ed41a6a01a14d90a876b5e5a0de6804018
Shoaib-78637 <syedshoaibabbas786@gmail.com>
2020-06-12 16:29:36 +0500

    React App

---
+++ src/lunch.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './lunch.css'
+
+function Lunch(props){
+    return(
+ 
+        <div>
+            <h1>Lunch  Manu</h1>
+            <h3> You Order = {props.Foods}</h3>
+                  <h3>Price = { props.Price }</h3>
+               
+               
+                
+           
+        </div>
+
+    );
+
+}
+export default Lunch;
