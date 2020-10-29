cafe41742e0d6e26b32c6e1fc16dce4662c888d1
AWAIS-AHMED-SYS <55208893+AWAIS-AHMED-SYS@users.noreply.github.com>
2020-10-29 20:51:57 +0500

    Update lunch.js
    receving

--- src/lunch.js
+++ src/lunch.js
@@ -5,16 +5,16 @@ function Lunch(props){
     return(
  
         <div>
-            <h1>Lunch  Manu</h1>
-            <h3> You Order = {props.Foods}</h3>
+            <h1>Lunch  Manu</h1>  
+            <h3> You Order = {props.Foods }</h3>
                   <h3>Price = { props.Price }</h3>
                
                
-                
+                 
            
         </div>
 
     );
-
+//reciving props 
 }
 export default Lunch;
